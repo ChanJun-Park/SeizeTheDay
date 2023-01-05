4df18cb4c888e3e76df1f3629f5071f5c67d5505
chanjun.park <pcj01142@gmail.com>
2023-01-05 21:11:24 +0900

    초기 의존성 추가

---
+++ app/build.gradle.kts
@@ -0,0 +1,135 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+kapt {
+    correctErrorTypes = true
+}
+
+android {
+    namespace = ProjectConfig.appId
+    compileSdk = ProjectConfig.compileSdk
+
+    defaultConfig {
+        applicationId = ProjectConfig.appId
+        minSdk = ProjectConfig.minSdk
+        targetSdk = ProjectConfig.targetSdk
+        versionCode = ProjectConfig.versionCode
+        versionName = if (ProjectConfig.versionBuild == 0) {
+            ProjectConfig.versionName
+        } else {
+            ProjectConfig.versionFullName
+        }
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "${project.rootDir}/tools/proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Compose.composeCompilerVersion
+    }
+    packagingOptions {
+        resources.excludes.add("META-INF/AL2.0")
+        resources.excludes.add("META-INF/LGPL2.1")
+        resources.excludes.add("**/attach_hotspot_windows.dll")
+        resources.excludes.add("META-INF/licenses/ASM")
+    }
+}
+
+dependencies {
+
+    // AndroidX
+    implementation(AndroidX.coreKtx)
+    implementation(AndroidX.appCompat)
+
+    // LifeCycle
+    implementation(LifeCycle.runtime)
+
+    // Compose
+    implementation(Compose.compiler)
+    implementation(Compose.ui)
+    implementation(Compose.uiToolingPreview)
+    implementation(Compose.hiltNavigationCompose)
+    implementation(Compose.material)
+    implementation(Compose.runtime)
+    implementation(Compose.navigation)
+    implementation(Compose.viewModelCompose)
+    implementation(Compose.activityCompose)
+
+    // Dagger/Hilt
+    implementation(DaggerHilt.hiltAndroid)
+    kapt(DaggerHilt.hiltCompiler)
+
+    // Coil
+    implementation(Coil.coilCompose)
+
+    // Material
+    implementation(Google.material)
+
+    // Retrofit
+    implementation(Retrofit.okHttp)
+    implementation(Retrofit.retrofit)
+    implementation(Retrofit.okHttpLoggingInterceptor)
+    implementation(Retrofit.moshiConverter)
+
+    // Room
+    kapt(Room.roomCompiler)
+    implementation(Room.roomKtx)
+    implementation(Room.roomRuntime)
+
+    // Test
+    testImplementation(Testing.junit4)
+    testImplementation(Testing.junitAndroidExt)
+    testImplementation(Testing.truth)
+    testImplementation(Testing.coroutines)
+    testImplementation(Testing.turbine)
+    testImplementation(Testing.composeUiTest)
+    testImplementation(Testing.mockk)
+    testImplementation(Testing.mockWebServer)
+
+    androidTestImplementation(Testing.junit4)
+    androidTestImplementation(Testing.junitAndroidExt)
+    androidTestImplementation(Testing.truth)
+    androidTestImplementation(Testing.coroutines)
+    androidTestImplementation(Testing.turbine)
+    androidTestImplementation(Testing.composeUiTest)
+    androidTestImplementation(Testing.mockkAndroid)
+    androidTestImplementation(Testing.mockWebServer)
+    androidTestImplementation(Testing.hiltTesting)
+    kaptAndroidTest(DaggerHilt.hiltCompiler)
+    androidTestImplementation(Testing.testRunner)
+
+    debugImplementation(Compose.uiTooling)
+    debugImplementation(Compose.testManifest)
+}
---
+++ buildSrc/src/main/java/LifeCycle.kt
@@ -0,0 +1,13 @@
+object LifeCycle {
+	private const val lifeCycleVersion = "2.5.1"
+
+	const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifeCycleVersion"
+	const val viewModelSavedState = "androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifeCycleVersion"
+	const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:$lifeCycleVersion"
+	const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:$lifeCycleVersion"
+	const val common = "androidx.lifecycle:lifecycle-common-java8:$lifeCycleVersion"
+	const val service = "androidx.lifecycle:lifecycle-service:$lifeCycleVersion"
+	const val process = "androidx.lifecycle:lifecycle-process:$lifeCycleVersion"
+	const val reactivestreams = "androidx.lifecycle:lifecycle-reactivestreams:$lifeCycleVersion"
+	const val runtime = "androidx.lifecycle:lifecycle-runtime-ktx:$lifeCycleVersion"
+}
