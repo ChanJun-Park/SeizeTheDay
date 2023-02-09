9d942e3cbfd2cb0d8254a75e9582cd11e624f27c
chanjun.park <pcj01142@gmail.com>
2023-02-09 09:48:15 +0900

    첨부 이미지 모델 수정

--- app/src/main/java/com/jingom/seizetheday/data/model/AttachedImageEntity.kt
+++ app/src/main/java/com/jingom/seizetheday/data/model/AttachedImageEntity.kt
@@ -2,22 +2,23 @@ package com.jingom.seizetheday.data.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.jingom.seizetheday.domain.model.AttachedImage
 
 private const val THANKS_RECORD_ID_COLUMN_NAME = "thanks_record_id"
 private const val IMAGE_URL_COLUMN_NAME = "image_uri"
 
-@Entity(
-	tableName = "attached_image",
-	primaryKeys = [THANKS_RECORD_ID_COLUMN_NAME, IMAGE_URL_COLUMN_NAME]
-)
+@Entity(tableName = "attached_image")
 data class AttachedImageEntity(
+	@PrimaryKey(autoGenerate = true)
+	@ColumnInfo(name = "id")
+	val id: Int,
 	@ColumnInfo(name = THANKS_RECORD_ID_COLUMN_NAME)
 	val thanksRecordId: Int,
 	@ColumnInfo(name = IMAGE_URL_COLUMN_NAME)
 	val imageUri: String
 ) {
-	fun toDomainModel(): AttachedImage = AttachedImage(thanksRecordId, imageUri)
+	fun toDomainModel(): AttachedImage = AttachedImage(id, thanksRecordId, imageUri)
 }
 
-fun AttachedImage.toDataModel(): AttachedImageEntity = AttachedImageEntity(thanksRecordId, imageUri)
+fun AttachedImage.toDataModel(): AttachedImageEntity = AttachedImageEntity(id, thanksRecordId, imageUri)
--- app/src/main/java/com/jingom/seizetheday/domain/model/AttachedImage.kt
+++ app/src/main/java/com/jingom/seizetheday/domain/model/AttachedImage.kt
@@ -1,6 +1,7 @@
 package com.jingom.seizetheday.domain.model
 
 data class AttachedImage(
+	val id: Int,
 	val thanksRecordId: Int,
 	val imageUri: String
 )
