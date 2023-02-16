fbea5d994c16273d63927e279752003ae41c41aa
박찬준 <pcj01142@gmail.com>
2023-02-16 23:10:58 +0900

    앨범 선택 화면 구현

--- app/src/main/java/com/jingom/seizetheday/core/ui/LocalImagePickerScreen.kt
+++ app/src/main/java/com/jingom/seizetheday/core/ui/LocalImagePickerScreen.kt
@@ -8,10 +8,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,17 +27,25 @@ import coil.compose.AsyncImage
 import com.jingom.seizetheday.R
 import com.jingom.seizetheday.core.isInspectionMode
 import com.jingom.seizetheday.data.media.model.MediaImage
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import com.jingom.seizetheday.data.media.model.MediaImageAlbum
+
+sealed class LocalImagePickerUiState {
+	data class ImageSelectionState(
+		val imageList: List<MediaImage> = emptyList(),
+		val selectedImageList: List<MediaImage> = emptyList(),
+	): LocalImagePickerUiState()
+
+	data class AlbumSelectionState(
+		val albumList: List<MediaImageAlbum> = emptyList()
+	): LocalImagePickerUiState()
+}
 
 @Composable
 fun LocalImagePickerScreen(
-	imageList: List<MediaImage>,
-	modifier: Modifier = Modifier
+	localImagePickerUiState: LocalImagePickerUiState,
+	modifier: Modifier = Modifier,
+	onImageClick: (MediaImage) -> Unit = {},
+	onAlbumClick: (MediaImageAlbum) -> Unit = {}
 ) {
 	Surface(modifier = modifier.fillMaxSize()) {
 		Box(modifier = Modifier.fillMaxSize()) {
@@ -41,20 +57,19 @@ fun LocalImagePickerScreen(
 					.background(color = MaterialTheme.colors.surface.copy(alpha = 0.1f))
 			)
 
-			LazyVerticalGrid(
-				columns = GridCells.Adaptive(minSize = 100.dp),
-				verticalArrangement = Arrangement.spacedBy(5.dp),
-				horizontalArrangement = Arrangement.spacedBy(5.dp),
-				contentPadding = PaddingValues(top = 65.dp),
-				modifier = Modifier.fillMaxSize()
-			) {
-				items(
-					items = imageList,
-					key = MediaImage::id
-				) {
-					SingleLocalImage(
-						mediaImage = it,
-						modifier = Modifier.aspectRatio(1f)
+			when (localImagePickerUiState) {
+				is LocalImagePickerUiState.ImageSelectionState -> {
+					ImageList(
+						imageList = localImagePickerUiState.imageList,
+						onImageClick = onImageClick,
+						modifier = Modifier.fillMaxSize()
+					)
+				}
+				is LocalImagePickerUiState.AlbumSelectionState -> {
+					AlbumList(
+						albumList = localImagePickerUiState.albumList,
+						onAlbumClick = onAlbumClick,
+						modifier = Modifier.fillMaxSize()
 					)
 				}
 			}
@@ -64,7 +79,7 @@ fun LocalImagePickerScreen(
 
 @Preview
 @Composable
-fun LocalImagePickerScreenPreview() {
+private fun LocalImagePickerScreenPreview() {
 	val dummyImageList = listOf(
 		MediaImage(1, 1, Uri.EMPTY, "image/png", 0, 0),
 		MediaImage(2, 2, Uri.EMPTY, "image/png", 0, 0),
@@ -75,8 +90,107 @@ fun LocalImagePickerScreenPreview() {
 		MediaImage(7, 7, Uri.EMPTY, "image/png", 0, 0),
 		MediaImage(8, 8, Uri.EMPTY, "image/png", 0, 0),
 	)
+	val localImagePickerUiState = LocalImagePickerUiState.ImageSelectionState(
+		imageList = dummyImageList
+	)
 	LocalImagePickerScreen(
-		imageList = dummyImageList,
+		localImagePickerUiState = localImagePickerUiState,
+		modifier = Modifier.fillMaxSize()
+	)
+}
+
+@Composable
+private fun ImageList(
+	imageList: List<MediaImage>,
+	modifier: Modifier = Modifier,
+	onImageClick: (MediaImage) -> Unit = {}
+) {
+	LazyVerticalGrid(
+		columns = GridCells.Adaptive(minSize = 100.dp),
+		verticalArrangement = Arrangement.spacedBy(5.dp),
+		horizontalArrangement = Arrangement.spacedBy(5.dp),
+		contentPadding = PaddingValues(top = 65.dp),
+		modifier = modifier.fillMaxSize()
+	) {
+		items(
+			items = imageList,
+			key = MediaImage::id
+		) {
+			SingleLocalImage(
+				mediaImage = it,
+				onClick = onImageClick,
+				modifier = Modifier.aspectRatio(1f)
+			)
+		}
+	}
+}
+
+@Composable
+private fun AlbumList(
+	albumList: List<MediaImageAlbum>,
+	modifier: Modifier = Modifier,
+	onAlbumClick: (MediaImageAlbum) -> Unit = {}
+) {
+	LazyColumn(modifier = modifier.fillMaxSize()) {
+		items(
+			items = albumList,
+			key = MediaImageAlbum::albumId
+		) {
+			SingleLocalImageAlbum(
+				imageAlbum = it,
+				onClick = onAlbumClick,
+				modifier = Modifier
+					.fillMaxWidth()
+					.height(60.dp)
+			)
+			Spacer(
+				modifier = Modifier
+					.fillMaxWidth()
+					.height(1.dp)
+					.background(color = Color.Gray.copy(alpha = 0.1f))
+			)
+		}
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AlbumListPreview() {
+	val dummyImageAlbumList = listOf(
+		MediaImageAlbum(
+			albumId = 1,
+			albumName = "Camera",
+			thumbnailImage = MediaImage(1, 1, Uri.EMPTY, "image/png", 0, 0),
+			imageCount = 10
+		),
+		MediaImageAlbum(
+			albumId = 2,
+			albumName = "Kakao",
+			thumbnailImage = MediaImage(2, 2, Uri.EMPTY, "image/png", 0, 0),
+			imageCount = 10
+		),
+		MediaImageAlbum(
+			albumId = 3,
+			albumName = "Naver",
+			thumbnailImage = MediaImage(3, 3, Uri.EMPTY, "image/png", 0, 0),
+			imageCount = 10
+		),
+		MediaImageAlbum(
+			albumId = 4,
+			albumName = "Calendar",
+			thumbnailImage = MediaImage(4, 4, Uri.EMPTY, "image/png", 0, 0),
+			imageCount = 10
+		),
+		MediaImageAlbum(
+			albumId = 5,
+			albumName = "DDD",
+			thumbnailImage = MediaImage(5, 5, Uri.EMPTY, "image/png", 0, 0),
+			imageCount = 10
+		)
+	)
+
+	AlbumList(
+		albumList = dummyImageAlbumList,
 		modifier = Modifier.fillMaxSize()
 	)
 }
@@ -107,7 +221,63 @@ private fun SingleLocalImage(
 }
 
 @Composable
-fun ImagePickerToolbar(
+private fun SingleLocalImageAlbum(
+	imageAlbum: MediaImageAlbum,
+	modifier: Modifier = Modifier,
+	onClick: (MediaImageAlbum) -> Unit = {}
+) {
+	Row(
+		modifier = modifier
+			.padding(
+				horizontal = 20.dp,
+				vertical = 10.dp
+			)
+			.clickable { onClick(imageAlbum) }
+	) {
+
+		SingleLocalImage(
+			mediaImage = imageAlbum.thumbnailImage,
+			modifier = Modifier
+				.fillMaxHeight()
+				.aspectRatio(1f)
+		)
+
+		Spacer(modifier = Modifier.width(10.dp))
+		
+		Column(
+			verticalArrangement = Arrangement.SpaceBetween,
+			modifier = Modifier.fillMaxSize()
+		) {
+			Text(
+				text = imageAlbum.albumName,
+				style = MaterialTheme.typography.body1
+			)
+			Text(
+				text = imageAlbum.imageCount.toString(),
+				style = MaterialTheme.typography.caption
+			)
+		}
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SingleLocalImageAlbumPreview() {
+	SingleLocalImageAlbum(
+		imageAlbum = MediaImageAlbum(
+			albumId = 1,
+			albumName = "Camera",
+			thumbnailImage = MediaImage(1, 1, Uri.EMPTY, "image/png", 0, 0),
+			imageCount = 30
+		),
+		modifier = Modifier
+			.fillMaxWidth()
+			.height(60.dp)
+	)
+}
+
+@Composable
+private fun ImagePickerToolbar(
 	modifier: Modifier = Modifier,
 	title: String = "카메라 롤",
 	onBackButtonClick: () -> Unit = {},
@@ -150,7 +320,7 @@ fun ImagePickerToolbar(
 
 @Preview
 @Composable
-fun ImagePickerToolbarPreview() {
+private fun ImagePickerToolbarPreview() {
 	ImagePickerToolbar(
 		modifier = Modifier
 			.fillMaxWidth()
---
+++ app/src/main/java/com/jingom/seizetheday/data/media/model/MediaImageAlbum.kt
@@ -0,0 +1,8 @@
+package com.jingom.seizetheday.data.media.model
+
+data class MediaImageAlbum(
+	val albumId: Int,
+	val albumName: String,
+	val thumbnailImage: MediaImage,
+	val imageCount: Int
+)
