e358ad0eb664113ceb04af6021d691f43ba3e84b
박찬준 <chanjun.park@navercorp.com>
2023-08-16 20:46:22 +0900

    리포맷

--- app/src/main/java/com/jingom/seizetheday/presentation/write/SelectingFeelingScreen.kt
+++ app/src/main/java/com/jingom/seizetheday/presentation/write/SelectingFeelingScreen.kt
@@ -2,7 +2,18 @@ package com.jingom.seizetheday.presentation.write
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,8 +24,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jingom.seizetheday.core.BackPressHandler
-import com.jingom.seizetheday.core.ui.VerticalScrollableContainer
 import com.jingom.seizetheday.core.ui.SimpleToolBar
+import com.jingom.seizetheday.core.ui.VerticalScrollableContainer
 import com.jingom.seizetheday.domain.model.Feeling
 import com.jingom.seizetheday.presentation.getResourceString
 import com.jingom.seizetheday.presentation.ui.theme.SeizeTheDayTheme
